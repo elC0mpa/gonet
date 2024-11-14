84e56f4a3fd46ef1ff4de9c714c7aaaf5ce0cc06
Evert Arias <evert.arias@treve.dev>
2024-11-14 17:08:17 -0500

    refactor: Improve table rendering and add console clear function

--- ui/table/table.go
+++ ui/table/table.go
@@ -3,37 +3,46 @@ package table
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"runtime"
 
 	"github.com/elC0mpa/gonet/model/network"
 	"github.com/jedib0t/go-pretty/v6/table"
 )
 
 type drawer struct {
-	table      table.Writer
-	rowsDrawed int
+	table table.Writer
 }
 
 func NewTableDrawer() TableDrawer {
 	t := table.NewWriter()
-	t.SetOutputMirror(os.Stdout)
 	t.AppendHeader(table.Row{"Application", "Bytes Rcvd", "Bytes Sent"})
 
 	return &drawer{
-		table:      t,
-		rowsDrawed: 0,
+		table: t,
 	}
 }
 
-func (tableDrawer *drawer) Draw(appUsage map[string]network.NetworkInfo) {
-	if tableDrawer.rowsDrawed > 0 {
-		tableDrawer.table.ResetRows()
+func clearConsole() {
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("cmd", "/c", "cls")
+	default:
+		cmd = exec.Command("clear")
 	}
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func (tableDrawer *drawer) Draw(appUsage map[string]network.NetworkInfo) {
+	tableDrawer.table.ResetRows()
 
 	for appName, netInfo := range appUsage {
 		tableDrawer.table.AppendRow(table.Row{appName, netInfo.ReceivedBytes, netInfo.SentBytes})
 	}
 
-	fmt.Println(tableDrawer.table.Render())
+	clearConsole()
 
-	tableDrawer.rowsDrawed = len(appUsage)
+	fmt.Println(tableDrawer.table.Render())
 }
